e20011f7229b6a48d44f1614a33d3560147715aa
Jaspal Singh <jsingh49@ucmerced.edu>
2016-12-05 21:42:31 -0800

    search displays the houses and redirects then to the page with house information

--- DBProject/search/views.py
+++ DBProject/search/views.py
@@ -26,9 +26,9 @@ def searchQ(request):
     c = conn.cursor()
     html = ''
 
-    for row in c.execute('SELECT distinct p_name, h_housekey FROM House, Pictures, Realtor, Manages, Reviews WHERE h_housekey = p_houseKey and r_realtorKey = m_realtor and r_realtorKey = rv_realtorkey and m_housekey = h_housekey and h_price >= ? and h_price <= ? and h_constructionyear >= ? and h_constructionyear <= ? and rv_rating >= ? and rv_rating <= ? and h_numRooms >= ? and h_numRooms <= ? and h_numBath >= ? and h_numBath <= ?',(minPrice,maxPrice,ayear, byear, lstar, hstar, minroom, maxroom, minbthm, maxbthm)):
+    for row in c.execute('SELECT distinct h_housekey,p_name FROM House as H, Pictures Where p_name in (SELECT p_name FROM House, Pictures, Realtor, Manages, Reviews WHERE H.h_housekey = h_housekey and h_housekey = p_houseKey and r_realtorKey = m_realtor and r_realtorKey = rv_realtorkey and m_housekey = h_housekey and h_price >= ? and h_price <= ? and h_constructionyear >= ? and h_constructionyear <= ? and rv_rating >= ? and rv_rating <= ? and h_numRooms >= ? and h_numRooms <= ? and h_numBath >= ? and h_numBath <= ? LIMIT 1)',(minPrice,maxPrice,ayear, byear, lstar, hstar, minroom, maxroom, minbthm, maxbthm)):
         if(row is not None):
-            html += "<a href=../home/ViewHouse?h_housekey=" + str(row[1]) + "><img src = \""+ str(row[0]) +"\" height=\"50\" width=\"50\" </img><br></a>"
+            html += "<a href=../home/ViewHouse?h_housekey=" + str(row[0]) + "><img src = \""+ str(row[1]) +"\" height=\"50\" width=\"50\" </img><br></a>"
         else:
             html += "<h1>None Found</h1>"
             break

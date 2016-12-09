778bff350b1764a9589bd0e69dcf242f85316cb3
Jaspal Singh <jsingh49@ucmerced.edu>
2016-12-08 21:21:06 -0800

    added more restrictions on the search in case they use anything other than strings

--- DBProject/DBProject/urls.py
+++ DBProject/DBProject/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('home.urls')),
     url(r'^search/', include('search.urls')),
+    url(r'^rsearch/', include('rsearch.urls')),
     url(r'^SignIn/', include('SignIn.urls')),
     url(r'^SignUp/', include('SignUp.urls')),
     url(r'^Account/', include('AccountPage.urls')),
